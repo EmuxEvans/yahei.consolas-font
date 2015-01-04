f17f63e030b66c2cb9f4a5f73f23b8b73c89d0a5
jerry <jerry@u32.(none)>
2015-01-04 21:30:12 +0800

    create

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo mkdir /usr/share/fonts/consolas
+
+sudo cp ./*.ttf /usr/share/fonts/consolas
+
+cd /usr/share/fonts/consolas
+
+sudo chmod 644 *.ttf
+
+sudo mkfontdir
+
+sudo mkfontscale
+
+sudo fc-cache -fv
+
